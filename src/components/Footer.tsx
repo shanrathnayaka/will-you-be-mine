67d1b49fc212db788d155a1ff0cb44b32aa9a3c7
shanrathnayaka <shanrathnayaka76@gmail.com>
2026-02-19 19:44:58 +0530

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Will You Be Mine. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
