ecd36d7d25aa33694aaa70aa463b4086bdfe1357
shanrathnayaka <shanrathnayaka76@gmail.com>
2026-02-19 19:44:41 +0530

    Create Hero component in Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Will You Be Mine?</h1>
+                <p>Your romantic journey starts here.</p>
+                <button>Get Started</button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
